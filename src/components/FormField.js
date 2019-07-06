184eb8bf48fd143c3c5f5b47fd1e212ef9a2fc42
Dev Ashish Saradhana <devashish.3093@gmail.com>
2019-07-06 16:23:42 +0530

    latest coder globant assignment react

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 // TODO: rewrite the same component as a functional component
 
-class FormField extends React.Component {
-  render() {
-    return (
-        <header>
-          <h2>{this.props.formTitle}</h2>
-        </header>
-    );
-  }
+const FormField = (props) => {
+	if(props.fieldType === "text") {
+		return (
+	        <div className="form-field">
+	        	<label className="input-label" htmlFor={props.name}>{props.labelName}</label>
+	        	<input className="input-field" type="text" id={props.name} name={props.name} onChange={(event) => props.valueChange(event)}/>
+	        </div>
+			)	
+	} else if(props.fieldType === "textarea") {
+		return (
+	        <div className="form-field">
+	        	<label className="input-label" htmlFor={props.name}>{props.labelName}</label>
+	        	<textarea className="input-field" type="text" id={props.name} name={props.name} onChange={(event) => props.valueChange(event)}/>
+	        </div>
+			)
+	}
 }
 
 export default FormField;
--- src/components/FormWrapper.js
+++ src/components/FormWrapper.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import FormField from './FormField';
 
-class FormWrapper extends React.Component {
+class FormWrapper extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      firstName : "",
+      email : "",
+      message : "",
+    }
+  }
+  valueChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  onDataSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(this.state));
+  }
   render() {
     return (
-      <form action="#">
-        <FormField formTitle={this.props.formTitle}/>
-        <div style={{width: '100%'}}>
-          <label style={{width: '50%', padding: '0 20px 0 0'}} class="desc" id="title1" for="Field1">Full Name</label>
-          <input style={{width: '50%'}} id="Field1" name="Field1" type="text" class="field text fn" value="" size="8" tabindex="1" />
-        </div>
-        <div style={{width: '100%'}}>
-          <label style={{width: '50%', padding: '0 20px 0 0'}} class="desc" id="title3" for="Field3">
-            Email
-          </label>
-          <input style={{width: '50%'}} id="Field3" name="Field3" type="email" spellcheck="false" value="" maxlength="255" tabindex="3"/> 
-        </div>
-        <div style={{width: '100%', margin: '20px 0 0 0'}}>
-          <label style={{width: '50%', padding: '0 20px 0 0'}} class="desc" id="title4" for="Field4">
-            Message
-          </label>
-            <textarea style={{width: '50%'}} id="Field4" name="Field4" spellcheck="true" rows="10" cols="50" tabindex="4"></textarea>
-        </div>
-        <div>
-          <div>
-            <div style={{width: '100%'}}>
-              <input id="Field5_0" name="Field5" type="submit" value="Submit" tabindex="5" />
-            </div>
-          </div>
-        </div>
-      </form>
-    );
+      <React.Fragment>
+        <h1>{this.props.formTitle}</h1>
+        <form action="#" onSubmit = {(e) => {
+          this.onDataSubmit(e);
+        }}>
+          <FormField fieldType={"text"} name={"firstName"} labelName={"First Name"} valueChange={this.valueChange} />
+          <FormField fieldType={"text"} name={"email"} labelName={"Email"} valueChange={this.valueChange} />
+          <FormField fieldType={"textarea"} name={"message"} labelName={"Message"} valueChange={this.valueChange} />
+          <input type="submit"/>
+        </form>
+      </React.Fragment>
+      );
   }
 }
 
